2ece1b7d14fa1bd3cec3c6a400115c433404c44e
RafidFahar <62782723+RafidFahar@users.noreply.github.com>
2020-10-18 19:07:49 +0700

    Initial Commit to GitHub

--- Employee/Controllers/EmployeesController.cs
+++ Employee/Controllers/EmployeesController.cs
@@ -47,7 +47,7 @@ namespace Employee.Controllers
         {
             return View();
         }
-        //.
+        //..
         // POST: Employees/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
